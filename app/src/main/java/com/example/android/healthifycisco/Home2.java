12c8a8b9c97911dd87a33c22a3d785038b508fc0
Mansi <mansibansal2000@gmail.com>
2019-06-03 12:24:53 +0530

    register

--- app/src/main/java/com/example/android/healthifycisco/Home2.java
+++ app/src/main/java/com/example/android/healthifycisco/Home2.java
@@ -5,21 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class Home2 extends AppCompatActivity {
-    private Button mSendData;
-    private Button profile;
+    private TextView mSendData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home2);
 
 
-        mSendData = (Button)findViewById(R.id.add_string);
-        profile = (Button)findViewById(R.id.login_btn) ;
+        mSendData = (TextView)findViewById(R.id.register);
         mSendData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -29,13 +28,6 @@ public class Home2 extends AppCompatActivity {
             }
         });
 
-        profile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(Home2.this,profile.class);
-                startActivity(i);
-                finish();
-            }
-        });
+
     }
 }
--- app/src/main/java/com/example/android/healthifycisco/Register.java
+++ app/src/main/java/com/example/android/healthifycisco/Register.java
@@ -1,69 +1,118 @@
 package com.example.android.healthifycisco;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class Register extends AppCompatActivity {
+public class Register extends AppCompatActivity implements View.OnClickListener{
 
-    EditText name,phone,email,username,password;
-    Button insert,back;
-    FirebaseDatabase database;
-    DatabaseReference ref;
+    TextView back;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private EditText editTextRePassword;
+    private Button buttonSignup;
+    private ProgressDialog progressDialog;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        back = (TextView)findViewById(R.id.login);
+        firebaseAuth = FirebaseAuth.getInstance();
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
 
-        name = (EditText)findViewById(R.id.name);
-        phone = (EditText)findViewById(R.id.phone);
-        email =(EditText)findViewById(R.id.email);
-        username = (EditText)findViewById(R.id.username_reg);
-        password = (EditText)findViewById(R.id.password_reg);
-        insert = (Button)findViewById(R.id.btn_submit_reg);
-        back =(Button)findViewById(R.id.btn_back_reg);
+        editTextRePassword = (EditText) findViewById(R.id.editTextrePassword);
 
-        database = FirebaseDatabase.getInstance();
-        ref = database.getReference("User");
+        buttonSignup = (Button) findViewById(R.id.buttonSignup);
+
+        progressDialog = new ProgressDialog(this);
+
+        //attaching listener to button
+        buttonSignup.setOnClickListener(this);
 
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(Register.this,Home2.class);
+                Intent i = new Intent(Register.this, Home2.class);
                 startActivity(i);
                 finish();
             }
         });
 
 
+    }
+
+    private void registerUser(){
+
+        //getting email and password from edit texts
+        String email = editTextEmail.getText().toString().trim();
+        String password  = editTextPassword.getText().toString().trim();
+
+        //checking if email and passwords are empty
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this,"Please enter email",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(this,"Please enter password",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+//        if(password != repassword){
+//            Toast.makeText(this,"Passwords don't match",Toast.LENGTH_LONG).show();
+//            return;
+//        }
+
+
+        //if the email and password are not empty
+        //displaying a progress dialog
+
+        progressDialog.setMessage("Registering Please Wait...");
+        progressDialog.show();
+
+        //creating a new user
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        //checking if success
+                        if(task.isSuccessful()){
+                            //display some message here
+                            Toast.makeText(Register.this,"Successfully registered",Toast.LENGTH_LONG).show();
+                        }else{
+                            //display some message here
+                            Toast.makeText(Register.this,"Registration Error",Toast.LENGTH_LONG).show();
+                        }
+                        progressDialog.dismiss();
+                    }
+                });
 
     }
 
-    public void btnInsert(View view){
-        ref.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        })
-
-
+    @Override
+    public void onClick(View v) {
+        registerUser();
     }
 }
